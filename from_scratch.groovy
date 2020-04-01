ae693a3e76093a354cca7cba4cf22ae49c039c4e
Andrii Panchuk <andpanchuk300@gmail.com>
2020-03-31 20:30:16 -0500

    .

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,55 +1,32 @@
-node { 
-
-properties([
-    // Below line sets "Discard Vuild more than 5"
-    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
-   // Below line triggers this job every minute
-    pipelineTriggers([pollSCM('* * * * *')])
-    ])
-
-
-stage("Pull Repo"){ 
-
-git 'https://github.com/farrukh90/cool_website.git' 
-
-} 
-
-stage("Install Prerequisites"){
+node {
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+		
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * *')])
+		])
+	stage("Pull Repo"){
+		git   'https://github.com/farrukh90/cool_website.git'
+	}
+	stage("Install Prerequisites"){
 		sh """
-		ssh centos@jenkins_worker1.cyber-pro.org                 sudo yum install httpd -y
-
-        """
-}
-        stage("Copy domain"){
-            """
-		scp -r *  centos@jenkins_worker1.cyber-pro.org:/tmp
-		ssh centos@jenkins_worker1.cyber-pro.org                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_worker1.cyber-pro.org                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_worker1.cyber-pro.org				   sudo chown centos:centos /var/www/html/
-		ssh centos@jenkins_worker1.cyber-pro.org				   sudo chmod 777 /var/www/html/*
-		ssh centos@jenkins_worker1.cyber-pro.org                sudo systemctl restart httpd 
+		ssh centos@dev1.acirrustech.com                 sudo yum install httpd -y
 		"""
+	}
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@dev1.cyber-pro.org:/tmp
+		ssh centos@dev1.cyber-pro.org                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@dev1.cyber-pro.org                sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@dev1.cyber-pro.org			   sudo chown centos:centos /var/www/html/
+		ssh centos@dev1.cyber-pro.org			   sudo chmod 777 /var/www/html/*
+		"""
+	}
+	stage("Restart web server"){
+		sh "ssh centos@dev1.cyber-pro.org                 sudo systemctl restart httpd"
+	}
+	stage("Slack"){
+		slackSend color: '#BADA55', message: 'Hello, World!'
+	}
 }
-
-} 
-
-stage("Restart web server"){ 
-    sh "ssh centos@jenkins_worker1.cyber-pro.org  sudo systemctl restart httpd"
-
-echo "hello" 
-
-} 
-
-stage("Stage4"){ 
-
-echo "hello" 
-
-} 
-
-stage("Stage5"){ 
-
-echo "hello" 
-
-} 
-
-} 
