f3b31d1c886991370f79630218a9805426bec3c3
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-02 15:07:38 -0500

    update

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -4,27 +4,33 @@ node {
 		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
 		
 		// Below line triggers this job every minute
-		pipelineTriggers([pollSCM('* * * * *')])
-		])
+		pipelineTriggers([pollSCM('* * * * *')]),
+		parameters([choice(choices: [
+			'dev1.cyber-pro.org', 
+			'qa1.cyber-pro.org', 
+			'stage1.cyber-pro.org', 
+			'prod1.cyber-pro.org'], 
+			description: 'Please choose an environment', 
+			name: 'ENVIR')]), 
 	stage("Pull Repo"){
 		git   'https://github.com/farrukh90/cool_website.git'
 	}
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@dev1.cyber-pro.org                  sudo yum install httpd -y
+		ssh centos@${ENVIR}                   sudo yum install httpd -y
 		"""
 	}
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@dev1.cyber-pro.org:/tmp
-		ssh centos@dev1.cyber-pro.org                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@dev1.cyber-pro.org                sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@dev1.cyber-pro.org			   sudo chown centos:centos /var/www/html/
-		ssh centos@dev1.cyber-pro.org			   sudo chmod 777 /var/www/html/*
+		scp -r *  centos@${ENVIR} :/tmp
+		ssh centos@${ENVIR}                  sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR} 			   sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR} 			   sudo chmod 777 /var/www/html/*
 		"""
 	}
 	stage("Restart web server"){
-		sh "ssh centos@dev1.cyber-pro.org                 sudo systemctl restart httpd"
+		sh "ssh centos@${ENVIR}                  sudo systemctl restart httpd"
 	}
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
