951ad1661a898a34e396ee81c3d92c7307aec04c
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-02 16:04:38 -0500

    update

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -12,26 +12,34 @@ node {
 			'prod1.cyber-pro.org'], 
 			description: 'Please choose an environment', 
 			name: 'ENVIR')]), 
+		])
+
+		// Pulls a repo from developer
 	stage("Pull Repo"){
 		git   'https://github.com/farrukh90/cool_website.git'
 	}
+		//Installs web server on different environment
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@${ENVIR}                   sudo yum install httpd -y
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
 		"""
 	}
+		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@${ENVIR} :/tmp
-		ssh centos@${ENVIR}                  sudo cp -r /tmp/index.html /var/www/html/
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
 		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@${ENVIR} 			   sudo chown centos:centos /var/www/html/
-		ssh centos@${ENVIR} 			   sudo chmod 777 /var/www/html/*
+		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
 		"""
 	}
+		//Restarts web server
 	stage("Restart web server"){
-		sh "ssh centos@${ENVIR}                  sudo systemctl restart httpd"
+		sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
 	}
+
+		//Sends a message to slack
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
 	}
