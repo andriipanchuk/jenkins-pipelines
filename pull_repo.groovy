6b8310f4257ccbe0b694aa7938f8781c68ff2875
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-04 12:37:24 -0500

    add script

--- pull_repo.groovy
+++ pull_repo.groovy
@@ -1,24 +1,32 @@
 node { 
 
 stage("Stage1"){ 
-echo "hello" 
-git 'https://github.com/farrukh90/packer.git'
+
+git 'https://github.com/farrukh90/packer.git' 
+
 } 
+
 stage("Stage2"){ 
+
 echo "hello" 
 
 } 
+
 stage("Stage3"){ 
+
 echo "hello" 
+
 } 
 
 stage("Stage4"){ 
+
 echo "hello" 
 
 } 
 
-stage("Send Notifications to Slack"){ 
-slackSend color: '#BADA55', message: 'Hello, World!' 
+stage("Stage5"){ 
+
+echo "hello" 
 
 } 
 
