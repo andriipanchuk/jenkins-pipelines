d1308177cfb0d6a36c1c87401e40ae8428b34d6e
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-04 13:08:11 -0500

    .

--- caller.groovy
+++ caller.groovy
@@ -1,36 +1,27 @@
- 
-
 node { 
 
 stage("Stage1"){ 
-
 echo "hello" 
 
 } 
 
 stage("Stage2"){ 
-
 echo "hello" 
 
 } 
 
 stage("Stage3"){ 
-
 echo "hello" 
 
 } 
 
 stage("Stage4"){ 
-
-echo "hello" 
-
-} 
-
-stage("Timestamp"){ 
-timestamps { 
-echo "hello" 
+build 'Template' 
 
 } 
+
+stage("Stage5"){ 
+echo "hello" 
 
 } 
 
