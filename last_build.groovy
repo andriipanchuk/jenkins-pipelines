238dac448ce67390663e367d49dfed776386a000
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-04 14:16:00 -0500

    .

---
+++ last_build.groovy
@@ -0,0 +1,34 @@
+node { 
+properties([
+  // keeps only last 5
+  buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+  
+  pipelineTriggers([
+    // runs with an input
+    upstream('Input'),
+    // execute every minute
+    cron('* * * * *')])  
+  ])
+  
+  stage("Stage1"){ 
+    git 'https://github.com/farrukh90/packer.git' 
+} 
+
+  stage("Stage2"){ 
+    echo "hello" 
+} 
+
+  stage("Stage3"){ 
+    echo "hello" 
+} 
+
+  stage("Stage4"){ 
+    echo "hello" 
+} 
+
+  stage("Stage5"){ 
+    node { 
+    // some block 
+    }  
+  } 
+} 
