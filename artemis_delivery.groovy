59618f1ea312a0802ecd47a964da0cef061af7e0
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-07 18:49:13 -0500

    update

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -53,5 +53,22 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 } 
 
 } 
+stage("Build Docker Image"){ 
+
+timestamps { 
+
+ws { 
+
+sh ''' 
+
+docker build -t artemis:${Version} . 
+
+''' 
+
+} 
+
+} 
+
+}
 
 }
