034b33444760d0ccefc73d7f3599e13f88f726ae
Andrii Panchuk <andpanchuk300@gmail.com>
2020-04-10 23:04:40 -0500

    /

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -74,16 +74,7 @@ name: 'ENVIR')])])
                         }
                 }
 
-                		stage("Authenticate"){
-			timestamps {
-				ws {
-					sh '''
-						ssh centos@${ENVIR} $(aws ecr get-login --no-include-email --region us-east-1)
-						'''
-			}
-		}
-	}
-
+                
 
 
 
@@ -111,12 +102,10 @@ name: 'ENVIR')])])
 		timestamps {
 			ws {
 				sh '''
-					ssh centos@.${ENVIR} docker run -dti -p 5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+					ssh centos@dev1.cyber-pro.org docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
 					'''
 				}
 			}
 		}
-      }
-
-
+        } 
   }
